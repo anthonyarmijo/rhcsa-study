46f8c7e405eaa29cde93a81af6f9b77072ed659a
Anthony Armijo <anthony.armijo@icloud.com>
2024-06-16 19:47:13 -0700

    updated initial script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sudo dnf -y install git
 # Clone the repository
 git clone https://github.com/anthonyarmijo/rhcsa-study.git
 git config --global credential.helper store
+git config --global user.name "Anthony Armijo"
+git config --global user.email "anthony.armijo@icloud.com"
